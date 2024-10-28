e3fd10cea6b283800efb72737b9d8d8b3b3b8aa0
MilosJovic23 <milos.mj992@gmail.com>
2024-10-28 15:42:36 +0100

    * homework

--- Domaci.php
+++ Domaci.php
@@ -7,10 +7,22 @@
 
 
     if( strtolower($ime) == "administrator" && $lozinka == "mojaSifraJeSigurna"){
-        echo "Dobro dosao administartore";
+        echo "Dobro dosao administartore"."<br>";
     }
 
 
+    $trenutnoVreme = date("H");
+
+
+    if ( $trenutnoVreme >= 5 && $trenutnoVreme < 12 ) {
+        echo "Jutro";
+    }
+    else if ( $trenutnoVreme >= 12 && $trenutnoVreme <= 20 ){
+        echo "Podne";
+    }
+    else {
+        echo "Noc";
+    }
 
 
 ?>
